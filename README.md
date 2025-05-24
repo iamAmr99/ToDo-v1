d5e3717cb1b0f740cc1180850e67051c6138b2cd
iamAmr99 <amradel5599@gmail.com>
2025-05-24 19:23:37 +0300

    uploding the project

---
+++ README.md
@@ -0,0 +1,21 @@
+# ToDo v1
+
+A simple To-Do list app built with plain HTML, CSS, and JavaScript.
+
+## Features
+
+- Add tasks
+- Delete tasks with fade-out animation
+- Data saved in browser localStorage
+- Responsive design for desktop and mobile
+
+## How to Use
+2. Type your task in the input field and click **Add**.
+3. Click the checkbox to mark a task as done; it will fade out and be removed.
+4. Your tasks are saved automatically and persist on page reload.
+
+## Tech Stack
+
+- HTML5
+- CSS3
+- JavaScript (ES6)
---
+++ src/js/script.js
@@ -0,0 +1,51 @@
+let todoAdd = document.getElementById("todoAdd");
+let todoDisplay = document.getElementById("todoDisplay");
+let todoInput = document.getElementById("todoInput");
+
+let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
+
+function renderList() {
+  todoDisplay.innerHTML = "";
+  for (let i = 0; i < todoList.length; i++) {
+    todoDisplay.innerHTML += `
+      <div class="form-check todo-task">
+        <input class="form-check-input todo-checkbox" type="checkbox" />
+        <label class="form-check-label">${todoList[i]}</label>
+      </div>`;
+  }
+}
+
+
+
+todoAdd.addEventListener("click", () => {
+  if (todoInput.value === "") {
+    todoInput.style.border = "3px solid red";
+    return;
+  } else {
+    todoInput.style.border = "";
+  }
+
+  todoList.push(todoInput.value);
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+  todoInput.value = "";
+  renderList();
+
+});
+
+addEventListener("click", (e) => {
+  if (e.target.classList.contains("todo-checkbox")) {
+    e.target.nextElementSibling.style.textDecoration = "line-through";
+    e.target.nextElementSibling.style.textDecorationThickness = "3px";
+
+    const taskText = e.target.nextElementSibling.textContent;
+    const index = todoList.indexOf(taskText);
+    if (index > -1) {
+      todoList.splice(index, 1);
+      localStorage.setItem("todoList", JSON.stringify(todoList));
+    }
+    e.target.closest(".todo-task").style.opacity = "0.5";
+    setTimeout(() => {
+      e.target.closest(".todo-task").remove();
+    }, 1000);
+  }
+});
